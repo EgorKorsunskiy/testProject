122f7adb32a26eb685db53ed69299a3bf19c2bb1
EgorKorsunskiy <georgykors@gmail.com>
2020-10-25 19:47:18 +0200

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,8 +16,7 @@ import { ImageDetailComponent } from './image-detail/image-detail.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    MatCardModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
